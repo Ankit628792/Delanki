c4cfd4554b8970522d5209b25b20d1f89bdbead3
Ankit Kumar <ankit@sugoilabs.com>
2022-11-12 15:01:11 +0530

    seo

--- components/Devs.tsx
+++ components/Devs.tsx
@@ -53,8 +53,10 @@ function Devs() {
             <h1 className='text-4xl lg:text-5xl font-semibold text-white tracking-wide text-center'>Ready to start the project?</h1>
             <p className='text-lg lg:text-xl text-gray-200 text-center'>Let's have a discussion on it</p>
             <div className='flex flex-col sm:flex-row sm:items-center gap-8 py-6 flex-wrap justify-center'>
-                {developers.map(data => <div key={data.id} className='flex items-center gap-2 bg-white py-3 px-4 rounded-lg'>
-                    <Image layout="responsive" width="56px" height="56px" src={data.profilePic} className='rounded-full border-4 shadow-lg' alt="" />
+                {developers.map(data => <div key={data.id} className='flex items-center gap-2 bg-white py-3 px-4 rounded-lg relative'>
+                    <div className='relative flex-shrink-0 w-14 h-14 rounded-full'>
+                        <Image layout="responsive" width="56px" height="56px" src={data.profilePic} className='rounded-full border-4 shadow-lg' alt="" />
+                    </div>
                     <div>
                         <a target="_blank" rel="noreferrer" className='text-lg cursor-pointer text-gray-800 hover:text-blue-500 font-medium' href={data.linkedin}>{data.name}</a>
                         <p className='text-gray-500 text-sm'>{data.designation}</p>
