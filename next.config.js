3168e20112add211b5e09c2b03b099fc7ac90710
Ankit Kumar <ankit@sugoilabs.com>
2023-07-23 17:38:31 +0530

    domain

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = withPWA({
   // next.js config
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'nain-devs.vercel.app', 'ankit-resume.netlify.app', 'res.cloudinary.com', 'delanki.vercel.app']
+    domains: ['localhost', 'nain-devs.vercel.app', 'ankit-resume.netlify.app', 'res.cloudinary.com', 'delanki.vercel.app', 'www.delanki.com', 'delanki.com']
   },
   i18n: {
     locales: ['en-US', 'fr', 'nl-NL'],
